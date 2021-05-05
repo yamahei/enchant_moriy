d667540aefc9829ab9de06b30ce7197041d3b55a
yamaorii <yamaorii@gmail.com>
2021-05-05 13:36:39 +0900

    音が出ない問題はenchant.jsのバージョンUPで解決、重ね合わせの問題も直った：ゲーム中のフラグ管理が狂っているのはなぜ？

--- registpwa.js
+++ registpwa.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
 	if ('serviceWorker' in navigator) {
-		const path_to_service_worke = "/enchant_moriy/pwabuilder-sw.js";
-		const register_option = {};//{scope: "."};
+		const path_to_service_worke = "pwabuilder-sw.js";
+		const register_option = {scope: "/enchant_moriy/"};
 		navigator.serviceWorker.register(path_to_service_worke, register_option)
 		.then(function(registration) {
 			console.log("serviceWorker registed.");
