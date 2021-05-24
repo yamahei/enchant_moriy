11adad64ebc4102aa249f4823a1d9fb72220a2b5
yamaorii <yamaorii@gmail.com>
2021-05-24 21:58:03 +0900

    service workerをTapRunnerに併せて修正：chrome上では「no matching service worker detected」になってるけどAndroid端末でオフラインキャッシュできたコード。。。謎

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -156,54 +156,28 @@ const files_to_cache = `
 });
 
 
+//Caching Files with Service Worker
+//https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
 
-
-
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-// const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-// const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE_STORAGE_NAME)
-      .then((cache) => cache.addAll(files_to_cache))
-      // .then((cache) => cache.add(offlineFallbackPage))
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_STORAGE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE_STORAGE_NAME);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_STORAGE_NAME)
+    .then(function(cache) {
+      return cache.addAll(files_to_cache);
+    })
+  );
 });
+
